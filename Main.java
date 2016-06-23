b6571c84452c13a7fe471044969faca45401451b
Adam S <adam.stepien1@gmail.com>
2016-06-23 12:15:48 +0200

    polaczono dwie tablice stringow i wyswietlono imie i nazwisko

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String[] firstNames={"Adam", "Filip", "Piotr"};
+        String[] lastNames={"Jackowski", "Krychowiak", "Żmłudziński"};
+
+        String[][] firstLast={firstNames, lastNames}; //połączono obie tablice
+
+        System.out.println(firstLast[0][0]+" "+firstLast[1][0]);
     }
 }
