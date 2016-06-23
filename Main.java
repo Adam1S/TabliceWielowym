0970073c385350df69cc97447581c8c37c265938
Adam S <adam.stepien1@gmail.com>
2016-06-23 12:24:37 +0200

    wyswietlam rozmiary tablic oraz tablicy zawierajacej inne tablice

--- Main.java
+++ Main.java
@@ -10,5 +10,25 @@ public class Main {
         String[][] firstLast={firstNames, lastNames}; //połączono obie tablice
 
         System.out.println(firstLast[0][0]+" "+firstLast[1][0]);
+
+        int[] longTab=new int[1000];
+        longTab[99]=100;
+
+        int[] midTab=new int[100];
+        int[] smallTab=new int[10];
+
+        int[][] hugeTab=new int[3][];
+
+
+        hugeTab[0]=longTab;
+        hugeTab[1]=midTab;
+        hugeTab[2]=smallTab;
+
+        System.out.println("Długości tablic: ");
+        System.out.println(hugeTab[0].length+";"+hugeTab[1].length+";"+hugeTab[2].length);
+        System.out.println("Setny element tablicy longtab to: "+longTab[99]);
+        System.out.println("Wielkosc tablicy hugetab: "+hugeTab.length);
+
+
     }
 }
